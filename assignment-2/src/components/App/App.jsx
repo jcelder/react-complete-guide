f2f5c78909fc20300511ef9397bbbdd90e11f7f0
Josh <aresx130@gmail.com>
2018-03-26 06:42:26 -0700

    Add inputChangedHandler to handle input value and length

--- assignment-2/src/components/App/App.jsx
+++ assignment-2/src/components/App/App.jsx
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      inputText: '',
+      inputLength: 0,
+    }
+  }
+
+  inputChangedHandler = (event) => {
+    this.setState({
+      inputText: event.target.value,
+      inputLength: event.target.value.length,
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Assignment 2</h1>
+        <input type="text" value={this.state.inputText} onChange={this.inputChangedHandler} />
+        <p>Text Length: {this.state.inputLength}</p>
       </div>
     );
   }
