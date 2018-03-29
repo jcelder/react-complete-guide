269a9843a50d2daff75b14b26d159a9103565473
Josh <aresx130@gmail.com>
2018-03-28 23:02:47 -0700

    Convert to class based components

--- lectures/src/components/PersonsList/Person/Person.jsx
+++ lectures/src/components/PersonsList/Person/Person.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { Component } from 'react'
 import styles from './Person.css'
 
-const Person = (props) => {
-  return (
-    <div className={styles['container-person']}>
-      <p onClick={props.click}>My name is {props.name} and I am {props.age} years old!</p>
-      <p>{props.children}</p>
-      <input type="text" value={props.name} onChange={props.changed}/>
-    </div>
-  )
+class Person extends Component {
+  render() {
+    return (
+      <div className={styles['container-person']}>
+        <p onClick={this.props.click}>My name is {this.props.name} and I am {this.props.age} years old!</p>
+        <p>{this.props.children}</p>
+        <input type="text" value={this.props.name} onChange={this.props.changed}/>
+      </div>
+    )
+  }
 }
 export default Person
