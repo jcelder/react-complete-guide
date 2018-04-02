20db291e5df7fc981136e2803c916c5ce5962db2
Josh <aresx130@gmail.com>
2018-04-02 11:25:04 -0700

    Initial commit

---
+++ burger-builder/src/components/Layout/Layout.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Aux from '../../hoc/Aux'
+import styles from './Layout.css'
+
+const Layout = (props) => (
+  <Aux>
+    <div>Toolbar, SideDrawer, Backdrop</div>
+    <main className={styles.Content}>
+      {props.children}
+    </main>
+  </Aux>
+)
+
+export default Layout
