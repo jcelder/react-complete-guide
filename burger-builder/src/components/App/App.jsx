e7e7eeb3443befba43f978d131aafcd6c5665803
Josh <aresx130@gmail.com>
2018-04-02 11:26:09 -0700

    Add Layout and BurgerBuilder components to render method

--- burger-builder/src/components/App/App.jsx
+++ burger-builder/src/components/App/App.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+
+import Layout from '../Layout/Layout'
+import BurgerBuilder from '../../containers/BurgerBuilder/BurgerBuilder'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Burger Builder</h1>
+      <div>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
       </div>
     );
   }
