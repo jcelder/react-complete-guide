b698b76ad4f161cbea02df1e587925c784dc2ff5
Josh <aresx130@gmail.com>
2018-04-05 14:41:08 -0700

    Add NavigationItems component to Toolbar

--- burger-builder/src/components/Navigation/Toolbar/Toolbar.jsx
+++ burger-builder/src/components/Navigation/Toolbar/Toolbar.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import NavigationItems from '../NavigationItems/NavigationItems'
+
 import styles from './Toolbar.css'
 import Logo from '../../Logo/Logo'
 
@@ -8,7 +10,7 @@ const toolbar = props => (
     <div>MENU</div>
     <Logo />
     <nav>
-      ...
+      <NavigationItems />
     </nav>
   </header>
 )
