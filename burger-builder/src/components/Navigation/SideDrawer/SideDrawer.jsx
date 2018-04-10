6126ec60bac224b248c766cd4bd51d078e5e4671
Josh <aresx130@gmail.com>
2018-04-10 15:50:01 -0700

    Add Backdrop and Open and Close styling

--- burger-builder/src/components/Navigation/SideDrawer/SideDrawer.jsx
+++ burger-builder/src/components/Navigation/SideDrawer/SideDrawer.jsx
@@ -1,20 +1,29 @@
 import React from 'react'
 
+import Aux from '../../../hoc/Aux'
 import Logo from '../../Logo/Logo'
 import NavigationItems from '../NavigationItems/NavigationItems'
+import BackDrop from '../../UI/Backdrop/Backdrop'
 
 import styles from './SideDrawer.css'
 
 const sideDrawer = (props) => {
+  let attachedStyles = [styles.SideDrawer, styles.Close]
+  if (props.open) {
+    attachedStyles = [styles.SideDrawer, styles.Open]
+  }
   return(
-    <div className={styles.SideDrawer}>
-      <div className={styles.Logo}>
-        <Logo />
+    <Aux>
+      <BackDrop show={props.open} clicked={props.closed} />
+      <div className={attachedStyles.join(' ')} /* onClick={props.closed}*/>
+        <div className={styles.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    </Aux>
   )
 }
 
