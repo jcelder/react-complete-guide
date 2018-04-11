38ead547a0ab992092602a4b5c16c1aaa1da4fe3
Josh <aresx130@gmail.com>
2018-04-11 15:33:18 -0700

    Complete AJAX lectures

--- ajax/src/components/NewPost/NewPost.js
+++ ajax/src/components/NewPost/NewPost.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 import './NewPost.css';
 
@@ -7,6 +8,23 @@ class NewPost extends Component {
         title: '',
         content: '',
         author: 'Max'
+    }
+
+    resetState = () => {
+        this.setState({
+            title: '',
+            content: '',
+            author: 'Max'
+        })
+    }
+
+    postDataHandler = async () => {
+       await axios.post('/posts', {
+            title: this.state.title,
+            body: this.state.content,
+            author: this.state.author
+        })
+        this.resetState()
     }
 
     render () {
@@ -22,7 +40,7 @@ class NewPost extends Component {
                     <option value="Max">Max</option>
                     <option value="Manu">Manu</option>
                 </select>
-                <button>Add Post</button>
+                <button onClick={this.postDataHandler}>Add Post</button>
             </div>
         );
     }
--- ajax/src/containers/Blog/Blog.js
+++ ajax/src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from '../../axios'
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,16 +7,61 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            posts: [],
+            selectedPostId: null,
+            errors: false
+        }
+    }
+
+    fetchAllPosts = async () => {
+        try{
+            const response = await axios.get('/posts/')
+            const posts = response.data.slice(0,4)
+            const updatedPosts = posts.map(post => {
+                return {
+                    ...post,
+                    author: 'Josh'
+                }
+            })
+            this.setState({
+                posts: updatedPosts
+            })
+        } catch (error) {
+            this.setState({ errors : true })
+        }
+    }
+
+    postSelectedHandler = (postId) => {
+        this.setState({ selectedPostId: postId })
+    }
+
+    componentDidMount() {
+        this.fetchAllPosts()
+    }
+
     render () {
+        let posts = <p style={{ textAlign: 'center' }}>Something went wrong!</p>
+        if (!this.state.errors) {
+            posts = this.state.posts.map(post => {
+                return <Post 
+                    key={post.id} 
+                    title={post.title} 
+                    author={post.author} 
+                    clicked={() => this.postSelectedHandler(post.id)}/>
+            })
+        }
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                   {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />
