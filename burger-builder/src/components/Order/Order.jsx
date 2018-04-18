7b858f8359d9c087bdbcbbfc585934933f5e40f3
Josh <aresx130@gmail.com>
2018-04-17 21:56:39 -0700

    Replace placeholder with dynamic order information

--- burger-builder/src/components/Order/Order.jsx
+++ burger-builder/src/components/Order/Order.jsx
@@ -2,11 +2,28 @@ import React from 'react'
 
 import styles from './Order.css'
 
-const order = (props) => (
-  <div className={styles.Order}>
-    <p>Ingredients: Salad (1)</p>
-    <p>Price: <strong>USD 5.45</strong></p>
-  </div>
-)
+const order = (props) => {
+  const ingredients = []
+
+  for (let ingredientName in props.ingredients) {
+    ingredients.push(
+      {
+        name: ingredientName,
+        amount: props.ingredients[ingredientName]
+      }
+    )
+  }
+
+  const ingredientOutput = ingredients.map(ing => {
+    return <span key={ing.name} className={styles.Ingredients}>{ing.name} ({ing.amount}) </span>
+  })
+
+  return (
+    <div className={styles.Order}>
+      <p>Ingredients: {ingredientOutput}</p>
+      <p>Price: <strong>USD {props.price.toFixed(2)}</strong></p>
+    </div>
+  )
+}
 
 export default order
