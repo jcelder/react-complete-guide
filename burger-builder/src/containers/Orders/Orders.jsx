197d73da4a643383c2f439b4df3e4ff3e0a27b67
Josh <aresx130@gmail.com>
2018-04-17 21:57:11 -0700

    Add fetchOrders method to fetch orders from Firebase

--- burger-builder/src/containers/Orders/Orders.jsx
+++ burger-builder/src/containers/Orders/Orders.jsx
@@ -1,16 +1,51 @@
 import React, { Component } from 'react'
 
 import Order from '../../components/Order/Order'
+import axios from '../../axios-orders'
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler'
 
 class Orders extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      orders: [],
+      loading: true
+    }
+  }
+  componentDidMount() {
+    this.fetchOrders()
+  }
+
+  fetchOrders = async () => {
+    try{
+      const response = await axios.get('/orders.json')
+      const fetchedOrders = []
+      for (let key in response.data) {
+        fetchedOrders.push({
+          ...response.data[key],
+          id: key
+        })
+      }
+      this.setState({ orders: fetchedOrders, loading: false})
+    } catch (e) {
+      this.setState({ loading: false })
+      console.log(e)
+    }
+  }
+
   render() {
     return(
       <div>
-        <Order />
-        <Order />
+        {this.state.orders.map(order => (
+          <Order
+            key={order.id}
+            ingredients={order.ingredients}
+            price={+order.price} />
+        ))}
       </div>
     )
   }
 }
 
-export default Orders
+export default withErrorHandler(Orders, axios)
