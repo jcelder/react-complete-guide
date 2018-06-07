b9211b1e77f3f0ed9109d5638cfd468923356d49
Josh <aresx130@gmail.com>
2018-06-07 13:22:14 -0700

    Add redux-thunk

--- burger-builder/src/index.js
+++ burger-builder/src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
+import reduxThunk from 'redux-thunk'
+
 import './index.css';
 import App from './components/App/App.jsx';
 import reducer from './store/reducer'
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(reducer)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(reducer, composeEnhancers(applyMiddleware(reduxThunk)))
 
 ReactDOM.render(
 <Provider store={store}>
